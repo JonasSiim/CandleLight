dede2d903a6ab5c827ecfadadce93d537d08ddea
Jonas <jsan16@student.aau.dk>
2019-03-02 18:12:55 +0100

    Player Functionality
    Added player controller, player movement, collision, tracking 2d camera

---
+++ Assets/Player_Movement.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player_Movement : MonoBehaviour
+{
+    public CharacterController2D controller;
+    float horizontalMove = 0f;
+    public float runSpeed = 40f;
+    bool jump = false;
+
+    // Update is called once per frame
+    void Update()
+    {
+        horizontalMove = Input.GetAxisRaw("Horizontal") * runSpeed;
+        if (Input.GetButtonDown("Jump"))
+        {
+            jump = true;
+        }
+    }
+
+    void FixedUpdate()
+    {
+        //move our character
+        controller.Move(horizontalMove * Time.fixedDeltaTime, false, jump);
+        jump = false;
+    }
+}
