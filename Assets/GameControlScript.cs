c599d175c3ddb68b196237145eb7927cb50adadb
Sexy Pancace 96 <michal.grzymski97@icloud.com>
2019-03-12 14:21:41 +0100

    Added Heath system
    Co-Authored-By: jonassiim <jonassiim@users.noreply.github.com>

---
+++ Assets/GameControlScript.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameControlScript : MonoBehaviour
+{
+    public GameObject LifePoint1, LifePoint2, LifePoint3;
+    public static int health;
+
+    public static int HeartScript { get; internal set; }
+    public static int RemoveLife { get; internal set; }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        health = 3;
+        LifePoint1.gameObject.SetActive(true);
+        LifePoint2.gameObject.SetActive(true);
+        LifePoint3.gameObject.SetActive(true);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (health > 3)
+            health = 3;
+
+        switch (health)
+        {
+            case 3:
+                LifePoint1.gameObject.SetActive(true);
+                LifePoint2.gameObject.SetActive(true);
+                LifePoint3.gameObject.SetActive(true);
+                break;
+            case 2:
+                LifePoint1.gameObject.SetActive(true);
+                LifePoint2.gameObject.SetActive(true);
+                LifePoint3.gameObject.SetActive(false);
+                break;
+            case 1:
+                LifePoint1.gameObject.SetActive(true);
+                LifePoint2.gameObject.SetActive(false);
+                LifePoint3.gameObject.SetActive(false);
+                break;
+            case 0:
+                LifePoint1.gameObject.SetActive(false);
+                LifePoint2.gameObject.SetActive(false);
+                LifePoint3.gameObject.SetActive(false);
+                break;
+
+        }
+    }
+}
---
+++ Assets/RemoveLife.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RemoveLife : MonoBehaviour
+{
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        GameControlScript.health -= 1;
+    }
+}
