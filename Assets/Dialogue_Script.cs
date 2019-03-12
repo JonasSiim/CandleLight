78de9d3b562472c7e980886f2ea93964f68d0579
Jonas Andersen <jsan16@student.aau.dk>
2019-03-12 13:08:21 +0100

    yo

---
+++ Assets/Dialogue_Script.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Dialogue_Script : MonoBehaviour
+{
+    // When player approaches NPC
+    void OnCollisionEnter(Collision col)
+    {
+        if(col.gameObject.tag == "Player")
+        {
+            Debug.Log("Press E to talk");
+        }
+    }
+
+}
