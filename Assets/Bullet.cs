7707b23beed54badc0b31c1ffb1c1d6006eb3af5
Sexy Pancace 96 <michal.grzymski97@icloud.com>
2019-03-26 14:56:37 +0100

    Attack/Enemyhit/EnemyDie added
    :)

---
+++ Assets/Bullet.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+
+    public float speed = 20f;
+    public Rigidbody2D rb;
+    public int damage = 40;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb.velocity = transform.right * speed;
+    }
+    void OnTriggerEnter2D(Collider2D hitInfo)
+    {
+        RemoveLife rat = hitInfo.GetComponent<RemoveLife>();
+        if (rat != null) {
+            rat.TakeDamage(damage);
+            Destroy(gameObject);
+        }
+          
+        Debug.Log(hitInfo.name);
+
+
+    }
+}
---
+++ Assets/Scripts/RangedAttack/MichalAI.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MichalAI : MonoBehaviour
+{
+    public Transform[] patrolPoints;
+    public float speed;
+    Transform currentPatrolPoint;
+    int currentPatrolIndex;
+
+    public Transform target;
+    public float chaseRange;
+
+    public float attackRange;
+    public int damage;
+    private float lastAttackTime;
+    public float attackDelay;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentPatrolIndex = 0;
+        currentPatrolPoint = patrolPoints[currentPatrolIndex];
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        float distanceToPlayer = Vector3.Distance(transform.position, target.position);
+        if (distanceToPlayer < attackRange) {
+            Vector3 targetDir = target.position - transform.position;
+            float angle = Mathf.Atan2(targetDir.y, targetDir.x) * Mathf.Rad2Deg - 90f;
+            Quaternion q = Quaternion.AngleAxis(angle, Vector3.forward);
+            transform.rotation = Quaternion.RotateTowards(transform.rotation, q, 90 * Time.deltaTime);
+        }
+    }
+}
