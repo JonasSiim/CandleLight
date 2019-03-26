4b3841cb8171607d75280eef554a683636f333d2
Jonas Andersen <jsan16@student.aau.dk>
2019-03-26 15:15:02 +0100

    Dialogue plus keys (WIP)

--- Assets/Scripts/Dialouge/Dialogue_Script.cs
+++ Assets/Scripts/Dialouge/Dialogue_Script.cs
@@ -7,8 +7,10 @@ public class Dialogue_Script : MonoBehaviour
 {
     public GameObject pressUI;
     public GameObject DialogueText;
+    public string[] dialogueElements;
     private bool boolSwitch;
     private int dialogueCount;
+    public int presentChoice;
     // When player approaches NPC
     void OnTriggerEnter2D(Collider2D col)
     {
@@ -26,31 +28,33 @@ public class Dialogue_Script : MonoBehaviour
             pressUI.SetActive(false);
             DialogueText.SetActive(false);
             boolSwitch = false;
-            dialogueCount = 0;
+            dialogueCount = -1;
         }
     }
 
     void Update()
     {
+        if (dialogueCount > -1)
+        {
+            DialogueText.GetComponent<TextMesh>().text = dialogueElements[dialogueCount];
+        }
         if (boolSwitch == true)
         {
-            if (Input.GetKeyDown(KeyCode.E))
+            if (Input.GetKeyDown(KeyCode.E) && dialogueCount < dialogueElements.Length - 1)
             {
                 DialogueText.SetActive(true);
                 dialogueCount++;
             }
         }
-        if (dialogueCount > 1)
-        {
 
-            DialogueText.GetComponent<TextMesh>().text = "Do you suck tities or vegana?";
-            Debug.Log("yes i do");
-        }
-        else if (dialogueCount < 1)
+        if(dialogueCount == presentChoice)
         {
-            DialogueText.GetComponent<TextMesh>().text = "Hi you little bitch";
+            if (Input.GetKeyDown(KeyCode.Y)) { Debug.Log("Candle Saved"); } //close dialogue
+            else if (Input.GetKeyDown(KeyCode.N)) { Debug.Log("Candle not Saved"); } //close dialogue
+            
         }
-        Debug.Log(dialogueCount);
+        
+        
     }
 
 }
