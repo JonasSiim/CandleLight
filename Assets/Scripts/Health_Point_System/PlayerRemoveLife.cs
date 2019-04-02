697c6a5cd889b6ae24d427efd96fd79664396d97
Sexy Pancace 96 <michal.grzymski97@icloud.com>
2019-04-02 11:36:00 +0200

    Fixed scrpts + boss fight

--- Assets/Scripts/Health_Point_System/PlayerRemoveLife.cs
+++ Assets/Scripts/Health_Point_System/PlayerRemoveLife.cs
@@ -5,7 +5,11 @@ using UnityEngine;
 public class PlayerRemoveLife : MonoBehaviour
 {
  public  void OnTriggerEnter2D(Collider2D collision)
-    {
-        GameControlScript.health -= 1;
+   {
+        if (collision.gameObject.tag == "Player")
+        {
+            GameControlScript.health -= 1;
+        }
+       
     }
 }
