dc690aec7e51e99d2ef8b5b308604a8a5eda0568
Jonas Andersen <jsan16@student.aau.dk>
2019-04-07 22:36:13 +0200

    Added key scripts

---
+++ Assets/Door_Script.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class Door_Script : MonoBehaviour
+{
+    
+    public GameObject UI;
+    public GameObject KeyObject;
+    public bool PickedUp;
+    private string message;
+    public bool boolSwitch;
+
+
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.gameObject.name == "Player") //Once players enters the key, show unlock message and set switch to true
+        {
+            
+            UI.SetActive(true);
+            boolSwitch = true;
+           
+
+
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D col) //stop rendering the pick up message
+    {
+        if (col.gameObject.name == "Player")
+        {
+            UI.SetActive(false);
+
+        }
+    }
+
+    private void Update()
+    {
+        DoorUpdate();
+        PickedUp = KeyObject.GetComponent<Key_Script>().PickedUp; //collect the bool variable from key script
+    }
+
+    void DoorUpdate()
+    {
+        if(boolSwitch == true)//if players stands in the collider
+        {
+            message = "Press E to Unlock";
+            UI.GetComponent<TextMeshProUGUI>().text = message;
+        }
+        if (Input.GetKeyDown(KeyCode.E) && PickedUp == true) //if key is pressed and key is picked up
+        {
+
+            gameObject.SetActive(false);
+            message = "";
+
+        }
+    }
+}
---
+++ Assets/Key_Script.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class Key_Script : MonoBehaviour
+
+
+{
+    //public GameObject keyObject;
+    public GameObject UI;
+    //public string door;
+    public bool PickedUp;
+    private string message;
+    private bool boolSwitch;
+
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.gameObject.name == "Player")
+        {
+            UI.SetActive(true);
+            boolSwitch = true;
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D col)
+    {
+        if (col.gameObject.name == "Player")
+        {
+            UI.SetActive(false);
+            if (PickedUp == true)
+            {
+                message = "";
+            }
+
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        KeyUpdate();
+    }
+
+    void KeyUpdate()
+    {
+        if (boolSwitch == true)
+        {
+            message = "Press E to Pick Up";
+            UI.GetComponent<TextMeshProUGUI>().text = message;
+            UI.SetActive(true);
+            if (Input.GetKeyDown(KeyCode.E))
+            {
+                PickedUp = true;
+                message = "The key has been picked up";
+                gameObject.SetActive(false);
+
+            }
+
+        }
+    }
+}
+
