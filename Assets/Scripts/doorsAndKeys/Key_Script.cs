fa217a89dd9503026953f4c5f80c12e5def62851
Stabell <31987773+Stabell@users.noreply.github.com>
2019-04-09 15:13:53 +0200

    key2

---
+++ Assets/Scripts/doorsAndKeys/Key_Script.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class Key_Script : MonoBehaviour
+
+
+{
+    //public GameObject keyObject;
+    public GameObject UI;
+    //public string door;
+    public bool PickedUp;
+    private string message;
+    private bool boolSwitch;
+
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.gameObject.name == "Player")
+        {
+            UI.SetActive(true);
+            boolSwitch = true;
+            message = "Press E to Pick Up";
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D col)
+    {
+        if (col.gameObject.name == "Player")
+        {
+            UI.SetActive(false);
+            message = "";
+
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        KeyUpdate();
+    }
+
+    void KeyUpdate()
+    {
+        if (boolSwitch == true)
+        {
+            
+            UI.GetComponent<TextMeshProUGUI>().text = message;
+            UI.SetActive(true);
+            if (Input.GetKeyDown(KeyCode.E))
+            {
+                PickedUp = true;
+                message = "The key has been picked up";
+                gameObject.SetActive(false);
+                UI.SetActive(false);
+
+            }
+
+        }
+    }
+}
+
