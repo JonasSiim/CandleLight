df9879148868f5f648607570f1f816c4b4aa0231
Sexy Pancace 96 <michal.grzymski97@icloud.com>
2019-04-16 15:42:34 +0200

    changed from 3 to 6 lives (player tag)

--- Assets/Scripts/Health_Point_System/GameControlScript.cs
+++ Assets/Scripts/Health_Point_System/GameControlScript.cs
@@ -6,7 +6,7 @@ using UnityEngine.SceneManagement;
 public class GameControlScript : MonoBehaviour
 {
     [SerializeField] private string newLevel;
-    public GameObject LifePoint1, LifePoint2, LifePoint3;
+    public GameObject LifePoint1, LifePoint2, LifePoint3, LifePoint4, LifePoint5, LifePoint6;
     public static int health;
 
     public static int HeartScript { get; internal set; }
@@ -15,45 +15,85 @@ public class GameControlScript : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        health = 3;
+        health = 6;
         LifePoint1.gameObject.SetActive(true);
         LifePoint2.gameObject.SetActive(true);
         LifePoint3.gameObject.SetActive(true);
+        LifePoint4.gameObject.SetActive(true);
+        LifePoint5.gameObject.SetActive(true);
+        LifePoint6.gameObject.SetActive(true);
 
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (health > 3)
-            health = 3;
+        if (health > 6)
+            health = 6;
 
         switch (health)
         {
+
+
+            case 6:
+                LifePoint1.gameObject.SetActive(true);
+                LifePoint2.gameObject.SetActive(true);
+                LifePoint3.gameObject.SetActive(true);
+                LifePoint4.gameObject.SetActive(true);
+                LifePoint5.gameObject.SetActive(true);
+                LifePoint6.gameObject.SetActive(true);
+                break;
+            case 5:
+                LifePoint1.gameObject.SetActive(true);
+                LifePoint2.gameObject.SetActive(true);
+                LifePoint3.gameObject.SetActive(true);
+                LifePoint4.gameObject.SetActive(true);
+                LifePoint5.gameObject.SetActive(true);
+                LifePoint6.gameObject.SetActive(false);
+                break;
+            case 4:
+                LifePoint1.gameObject.SetActive(true);
+                LifePoint2.gameObject.SetActive(true);
+                LifePoint3.gameObject.SetActive(true);
+                LifePoint4.gameObject.SetActive(true);
+                LifePoint5.gameObject.SetActive(false);
+                LifePoint6.gameObject.SetActive(false);
+                break;
             case 3:
                 LifePoint1.gameObject.SetActive(true);
                 LifePoint2.gameObject.SetActive(true);
                 LifePoint3.gameObject.SetActive(true);
+                LifePoint4.gameObject.SetActive(false);
+                LifePoint5.gameObject.SetActive(false);
+                LifePoint6.gameObject.SetActive(false);
                 break;
             case 2:
                 LifePoint1.gameObject.SetActive(true);
                 LifePoint2.gameObject.SetActive(true);
                 LifePoint3.gameObject.SetActive(false);
+                LifePoint4.gameObject.SetActive(false);
+                LifePoint5.gameObject.SetActive(false);
+                LifePoint6.gameObject.SetActive(false);
                 break;
             case 1:
                 LifePoint1.gameObject.SetActive(true);
                 LifePoint2.gameObject.SetActive(false);
                 LifePoint3.gameObject.SetActive(false);
+                LifePoint4.gameObject.SetActive(false);
+                LifePoint5.gameObject.SetActive(false);
+                LifePoint6.gameObject.SetActive(false);
                 break;
             case 0:
                 LifePoint1.gameObject.SetActive(false);
                 LifePoint2.gameObject.SetActive(false);
                 LifePoint3.gameObject.SetActive(false);
+                LifePoint4.gameObject.SetActive(false);
+                LifePoint5.gameObject.SetActive(false);
+                LifePoint6.gameObject.SetActive(false);
                 Destroy(gameObject);
                 SceneManager.LoadScene(newLevel);
                 //Add things that happen when the player dies
                 break;
-
         }
     }
 }
